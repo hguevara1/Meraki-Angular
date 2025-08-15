f51c18a4906fce5d73d2b48e14783e18cc571adf
hguevara1 <hguevarasoto@gmail.com>
2025-08-15 22:06:35 +0200

    Diseño inicial de la pantalla de inicio (en progreso)

---
+++ app/frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  errorMessage: string = '';
+
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.errorMessage = 'Por favor completa todos los campos correctamente.';
+      return;
+    }
+
+    // Simulación de login exitoso
+    console.log('Login exitoso:', this.loginForm.value);
+    this.errorMessage = ''; // Limpia el mensaje si todo va bien
+  }
+  loginWithGoogle() {
+    console.log('Login con Google iniciado...');
+    // Aquí podrías integrar Firebase, OAuth, etc.
+  }
+}
+
